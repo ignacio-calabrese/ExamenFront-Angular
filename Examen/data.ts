d0d8f7dcfd3cc1fdff25e93875bd5615dcee3334
ignacio-calabrese <ignacio-calabrese@hotmail.com>
2019-01-20 00:08:25 -0300

    solución del examen

---
+++ Examen/data.ts
@@ -0,0 +1,7 @@
+import { User } from './User';
+
+export const USERS: User[] = [
+  { id: 11, name: 'Tomás' , surname: 'Rodriguez', edad: 21,  profesion: 'Estudiante'},
+  { id: 12, name: 'María', surname: 'Figallo', edad: 33 , prefesion: 'Ingeniera Civil'},
+  { id: 13, name: 'Agustina' , surname: 'Leanz', edad: 42, profesion: 'Electricista'}
+];
